ee889ba6aeba45bb911bafa301cdd14e481c4262
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-05-12 11:32:58 +0600

    Implement pagination, search, and sorting

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -29,5 +29,66 @@ const getFoods = async (req, res) => {
   }
 }
 
+// get food by id
+const foodById = async (req, res) => {
+  try {
+    const food = await Food.findById(req.params.id)
+    res.status(200).json(food)
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+}
 
-module.exports = {createFoodCard, getFoods}
+// get pagination
+const getPagination = async (req, res) => {
+  try {
+    const size = parseInt(req.query.size) || 6;
+    const page = parseInt(req.query.page) || 1;
+    const filter = req.query.filter;
+    const sort = req.query.sort === "asc" ? 1 : req.query.sort === "dsc" ? -1 : null;
+    const search = req.query.search;
+
+
+    let query = {
+      foodName: { $regex: search, $options: "i" },
+    };
+    if (filter) query.foodStatus = filter;
+    
+    let options = {};
+    if (sort !== null) options.sort = { expiredDateTime: sort };
+
+    const foods = await Food.find(query).skip((page - 1) * size).limit(size).sort(options.sort);
+    res.status(200).json(foods);
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
+
+// data count from db
+const getDataCount = async (req, res) => {
+  try {
+    const filter = req.query.filter;
+    const search = req.query.search;
+     let query = {
+      foodName: { $regex: search, $options: "i" },
+    };
+    if (filter) query.foodStatus = filter;
+    const count = await Food.countDocuments(query);
+    res.status(200).json({ count });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
+
+
+
+module.exports = {createFoodCard, getFoods, foodById, getPagination, getDataCount}
--- routes/food.route.js
+++ routes/food.route.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { createFoodCard, getFoods } = require('../controllers/food.controller');
+const { createFoodCard, getFoods, foodById, getPagination, getDataCount } = require('../controllers/food.controller');
 
 const foodRouter = express.Router()
 
 foodRouter.post('/', createFoodCard)
 
 foodRouter.get('/', getFoods)
+
+foodRouter.get('/food-filter', getPagination)
+
+foodRouter.get('/food-count', getDataCount)
+
+foodRouter.get('/:id', foodById)
+
 
 module.exports = foodRouter;
