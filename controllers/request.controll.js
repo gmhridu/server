17c9fe924bc7bb9219ad4661f662d542ef87624c
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-05-14 13:56:59 +0600

    Implement endpoint to handle food requests

--- controllers/request.controll.js
+++ controllers/request.controll.js
@@ -1,16 +1,42 @@
 const Food = require("../models/food.model");
 const Request = require("../models/request.model");
 
+
+
 const requestedFood = async (req, res) => {
   try {
-    const requestBody = req.body;
-    const reqFood = new Request(requestBody);
-    const saveReqFood = await reqFood.save();
-    res.status(200).json(saveReqFood);
+    const { foodId, email, donatorEmail } = req.body;
+
+    const food = await Food.findById(foodId);
+
+    if (!food) {
+      return res.status(404).json({ message: "Food not found" });
+    }
+
+    food.foodQuantity = 0;
+    food.foodStatus = "Not Available";
+    await food.save();
+
+    const request = new Request({
+      ...req.body,
+      foodName: food.foodName,
+      foodImage: food.foodImage,
+      foodStatus: food.foodStatus,
+      expiredDateTime: food.expiredDateTime,
+      donatorEmail: food.donatorEmail,
+      donatorEmail,
+      email: email,
+    });
+
+    await request.save();
+
+    res.status(200).json(request);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
+
+
 
 const getAllReqFood = async (req, res) => {
   try {
@@ -38,7 +64,7 @@ const getReqFood = async (req, res) => {
 const getFoodDetailsByEmail = async (req, res) => {
   try {
     const email = req.params.email;
-    const foodDetails = await Request.find({ "donator.email": email });
+    const foodDetails = await Request.find({ 'donatorEmail': email });
     res.status(200).json(foodDetails);
   } catch (error) {
     res.status(500).json({ error: error.message });
--- models/request.model.js
+++ models/request.model.js
@@ -1,40 +1,48 @@
 const mongoose = require('mongoose');
 const Food = require('./food.model');
 
-const reqSchema = new mongoose.Schema({
-  foodName: {
-    type: String,
-    required: true
+const reqSchema = new mongoose.Schema(
+  {
+    foodName: {
+      type: String,
+      required: true,
+    },
+    foodImage: {
+      type: String,
+      required: true,
+    },
+    foodQuantity: {
+      type: Number,
+      required: true,
+    },
+    pickupLocation: {
+      type: String,
+      required: true,
+    },
+    expiredDateTime: {
+      type: Date,
+      required: true,
+    },
+    additionalNotes: String,
+    donatorEmail: String,
+    foodStatus: {
+      type: String,
+      enum: ["Available", "Not Available"],
+      default: "Available",
+    },
+    requestDate: {
+      type: Date,
+      default: Date.now,
+    },
+    email: String,
+    foodId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Food",
+      required: true,
+    },
   },
-  foodImage: {
-    type: String,
-    required: true
-  },
-  foodQuantity: {
-    type: Number,
-    required: true
-  },
-  pickupLocation: {
-    type: String,
-    required: true
-  },
-  expiredDateTime: {
-    type: Date,
-    required: true
-  },
-  additionalNotes: String,
-  donatorEmail: String,
-  foodStatus: {
-    type: String,
-    enum: ['Available', 'Not Available'],
-    default: 'Available'
-  },
-  requestDate: {
-    type: Date,
-    default: Date.now
-  },
-  email: String,
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Request = mongoose.model('Request', reqSchema);
 
